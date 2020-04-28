3445c092e02c48db0d49000087b86f35af5dc751
Zerofelx <zeroflex3@gmail.com>
2020-04-28 17:44:26 -0500

    Juego de la vida v0.1

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/colornames"
+)
+
+func run() {
+	cfg := pixelgl.WindowConfig{
+		Title:  "Juego de la vida",
+		Bounds: pixel.R(0, 0, 1024, 768),
+		VSync:  true,
+	}
+
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	win.Clear(colornames.Black)
+
+	for !win.Closed() {
+		win.Update()
+
+	}
+}
+
+func main() {
+	pixelgl.Run(run)
+
+}
